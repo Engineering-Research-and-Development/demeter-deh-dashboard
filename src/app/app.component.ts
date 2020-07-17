7db24fdbe483ece271f70517d49242cdadca366f
Slobodan Paunovic <slobodan.paunovic@eng.it>
2020-07-17 17:05:48 +0200

    Index.html splited into separate components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
+import { LoginModalComponent } from './components/modals/login-modal/login-modal.component';
 import { Component } from '@angular/core';
+import { faList, faEraser, faBars, faSearch, faAngleUp, faUser } from '@fortawesome/free-solid-svg-icons';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demeter-deh-dashboard';
+ 
+  angleUpIcon = faAngleUp;
+
+  constructor(private _modalService: NgbModal){}
+  cambia(config) {
+    console.log("test");
+
+  }
+ 
+
 }
