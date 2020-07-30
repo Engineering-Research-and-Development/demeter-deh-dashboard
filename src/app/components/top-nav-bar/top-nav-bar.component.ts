c80b4c4f12750b42ab857d9ec320fe8e2f74d5a5
Slobodan Paunovic <slobodan.paunovic@eng.it>
2020-07-30 12:50:15 +0200

    Logout from Keyrock added

--- src/app/components/top-nav-bar/top-nav-bar.component.ts
+++ src/app/components/top-nav-bar/top-nav-bar.component.ts
@@ -1,7 +1,5 @@
-import { environment } from './../../../environments/environment';
-import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { AuthService } from './../../services/auth.service';
 
 @Component({
   selector: 'app-top-nav-bar',
@@ -9,19 +7,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./top-nav-bar.component.css']
 })
 export class TopNavBarComponent implements OnInit {
-  constructor(private _router: Router, public authService: AuthService) { }
+  constructor(public authService: AuthService) { }
   ngOnInit(): void {
   }
 
-  getToken() {
-    window.location.href = `http://localhost:3000/oauth2/authorize?response_type=token&client_id=${environment.ID}&state=xyz&redirect_uri=http://localhost:4200/&scope=jwt`;
-  }
-
-  openLoginModal() {
-    // this._modalService.open(LoginModalComponent, { windowClass: 'modal-holder', centered: true });
-  }
-  openRegisterUserModal() {
-    // this._modalService.open(RegisterModalComponent, { windowClass: 'modal-holder', centered: true, size: 'lg' });
-  }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,17 @@
+import { Observable, throwError } from 'rxjs';
+import { environment } from './../../environments/environment';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { JwtModel } from './../models/jwt.model';
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from "@auth0/angular-jwt";
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
 
   get currentUser(): JwtModel {
     let token = this.getToken();
@@ -36,10 +40,50 @@ export class AuthService {
     return !isExpired;
   }
 
-  logout(): void {
+  removeToken(): void {
     if (localStorage.getItem("token")) {
       localStorage.removeItem("token");
     }
+  }
+
+  authorize() {
+    window.location.href = `http://localhost:3000/oauth2/authorize?response_type=token&client_id=${environment.ID}&state=xyz&redirect_uri=http://localhost:4200/&scope=jwt`;
+  }
+
+  clearKayrockSession() {
+    window.location.href = `http://localhost:3000/auth/external_logout?_method=DELETE&client_id=${environment.ID}`;
+    this.removeToken();
+  }
+
+  // clearKayrockSession(): Observable<any> {
+  //   const base = `${environment.ID}:${environment.SECRET}`;
+  //   return this._http.options(`http://localhost:3000/auth/external_logout?_method=DELETE`, {
+
+  //     params: {
+  //       "client_id": environment.ID
+  //     },
+  //     headers: {
+  //       // "Authorization": `Basic ${base}`
+  //       "Authorization": `Bearer ${this.getToken()}`
+  //     }
+  //   }).pipe(
+  //     catchError(this.handleError)
+  //   )
+  // }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error instanceof ErrorEvent) {
+      console.log(error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
   }
 
 }
