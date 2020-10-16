3305d393f2d6ecdd192c11700a61827494e642a9
Slobodan Paunovic <slobodan.paunovic@eng.it>
2020-10-16 15:23:22 +0200

    Show list view on sidebar list icon click

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,8 @@
 import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { UserInfo } from 'src/app/models/userInfo.model';
+declare let drawEntities: any;
+declare let resetDymerStart: any;
 
 @Component({
   selector: 'app-sidebar',
@@ -11,7 +13,7 @@ export class SidebarComponent implements OnInit {
   toggleClass: boolean;
   public roles: string[];
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService) {}
 
   ngOnInit(): void {
     if (this.authService.currentUser) {
@@ -24,5 +26,44 @@ export class SidebarComponent implements OnInit {
   }
   toggleSidebar() {
     this.toggleClass = !this.toggleClass;
+  }
+
+  //temp json config
+  jsonConfig = {
+    query: {
+      query: {
+        query: {
+          bool: {
+            must: [
+              {
+                term: {
+                  _index: 'dymerservicecomponent',
+                },
+              },
+            ],
+          },
+        },
+      },
+    },
+
+    endpoint: 'entity.search',
+    viewtype: 'teaserlist',
+    target: {
+      teaserlist: {
+        id: '#cont-MyList',
+        action: 'html',
+        reload: false,
+      },
+      fullcontent: {
+        id: '#cont-MyList',
+        action: 'html',
+      },
+    },
+  };
+
+  cambia(jj: any) {
+    var confbase = this.jsonConfig;
+    resetDymerStart();
+    drawEntities(jj);
   }
 }
