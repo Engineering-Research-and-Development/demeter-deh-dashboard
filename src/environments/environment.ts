3dc86f1b14a03890e7a974659561edb0f1d7267c
marest94 <stojanovic1994@yahoo.com>
2021-06-10 16:24:42 +0200

    Add Capability Manager URL in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
 
-  DYMER_URL: window["env"]["DYMER_URL"] || "http:localhost:8080"
+  DYMER_URL: window["env"]["DYMER_URL"] || "http:localhost:8080",
+  CAPABILITY_MANAGER_URL: window["env"]["CAPABILITY_MANAGER_URL"] || "https://acs.bse.h2020-demeter-cloud.eu:3030"
 
 };
 
