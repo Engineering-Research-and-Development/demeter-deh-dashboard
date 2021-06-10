984e57a68edc7d17defce64208089f037743f72c
marest94 <stojanovic1994@yahoo.com>
2021-06-10 16:30:43 +0200

    Small changes in Welcome page according to Usability report

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +14,28 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  isTablet: boolean;
+  // isTablet: boolean;
 
+  hideLoginParent: boolean;
+  @ViewChild(LoginComponent) private loginComp: LoginComponent;
+  @Output() isTablet = new EventEmitter<boolean>();
+  clicked: boolean;
+  
   constructor(public authService: AuthService) {}
 
   ngOnInit(): void {}
+
+  slideLogin() {
+    console.log("PARENT", this.loginComp)
+
+    this.loginComp.animateMe();
+    this.emitLoginClick();
+  }
+
+  emitLoginClick() {
+    if (screen.width >= 600 && screen.width <= 768) {
+      this.clicked = !this.clicked;
+      this.isTablet.emit(this.clicked);
+    }
+  }
 }
