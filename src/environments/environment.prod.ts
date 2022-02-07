21f1199b6b1201e779cc7d5e9bc303bf2aa3dfba
marest94 <stojanovic1994@yahoo.com>
2022-02-07 22:38:16 +0100

    Update env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  
-  DYMER_URL: window["env"]["DYMER_URL"] || "http:localhost:8080",
+
+  DYMER_URL: window["env"]["DYMER_URL"] || "http://localhost:8080",
   CAPABILITY_MANAGER_URL: window["env"]["CAPABILITY_MANAGER_URL"] || "https://acs.bse.h2020-demeter-cloud.eu:3030"
 
 };
