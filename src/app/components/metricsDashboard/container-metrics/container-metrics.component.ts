b447b58075349f87e36fef2bae2f9be582b3e2c5
marest94 <stojanovic1994@yahoo.com>
2022-02-07 23:04:35 +0100

    Update ContainerMetrics component

--- src/app/components/metricsDashboard/container-metrics/container-metrics.component.ts
+++ src/app/components/metricsDashboard/container-metrics/container-metrics.component.ts
@@ -2,12 +2,11 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { MetricsService } from '../../../services/metrics.service'
-import { metricsContainer } from '../../../models/metricsContainer'
 import { metricsData } from '../../../models/metricsData'
 import { DatePipe } from '@angular/common';
 import { ActivatedRoute, Router } from "@angular/router";
 import { AuthService } from 'src/app/services/auth.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-container-metrics',
@@ -15,21 +14,21 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./container-metrics.component.css']
 })
 export class ContainerMetricsComponent implements OnInit {
+  public state: any;
   containerMetrics: any;
   metrcisByRrm: metricsData;
-
-
   cpu_res: any;
   mem_res: any;
   CONTAINERS_DATA: any;
-
-  public state: any;
   multi: any[];
   view: any[] = [700, 400];
 
   // Color scheme to be used for cpu & memory utilization monitoring graphs.
   colorscheme = [
-    '#4DD0E1', '#A84DE1', '#E15E4D', '#86E14D', '#FFFD43', '#FF9F43', '#E81E74', '#AB81C3', '#95F3F5', '#4BE39A', '#0D439B', '#8FA618'
+    '#4DD0E1', '#A84DE1', '#E15E4D', '#86E14D', '#FFFD43', '#FF9F43', '#E81E74', '#AB81C3', '#95F3F5', '#4BE39A', '#0D439B', '#8FA618', '#C4A31F', '#F8DA9C', '#878DA8', '#FBB048',
+    '#B21728', '#27090B', '#E89AC6', '#EAD726', '#D57F1C', '#D1F0B8', '#BF705C', '#9ABFE2', '#4B3C4A', '#4E7803', '#F5EBC4', '#C1D80D', '#A4C0B9', '#4E7803', '#C25FD0', '#D67E1F',
+    '#131FD9', '#487859', '#80AF98', '#73423A', '#0A5B2C', '#E5B16A', '#F42070', '#AFE096', '#4C39FD', '#8E8909', '#77ACBD', '#185034', '#8CC2F8', '#DCFC9F', '#B5B731', '#9D6331',
+    '#A4471F', '#292CDA'
   ]
   displayedColumns: string[] = ['containerName', 'ipv4', 'uptime', 'host'];
   dataSource = new MatTableDataSource<ContainerData>();
@@ -49,7 +48,6 @@ export class ContainerMetricsComponent implements OnInit {
     filterValue = filterValue.toLowerCase();
     this.dataSource.filter = filterValue;
   }
-
 
   // ngx-chart options
   showXAxis: boolean = true;
@@ -80,17 +78,25 @@ export class ContainerMetricsComponent implements OnInit {
 
   ngOnInit(): void {
 
+    if (this.authService.isExpired()) {
+      this.authService.removeToken();
+      this.authService.removeCapToken();
+      this.router.navigateByUrl('');
+    }
+
     if (!this.authService.isLoggedIn) {
       this.router.navigateByUrl('');
     }
-    // rrmId
+
+
     this.state = window.history.state.container;
-    this.containerMetrics = this.state;
-    this.proba(this.containerMetrics)
-    // this.getMetricsByRrmId(this.state);
+    if (this.state != undefined) {
+      this.containerMetrics = this.state;
+      this.getMetricsFromInput(this.containerMetrics)
+    } else {
+      this.router.navigateByUrl('user-metrics');
+    }
   }
-
-
 
   axisDigits(val: any): any {
     return new DatePipe('en').transform(val);
@@ -101,8 +107,7 @@ export class ContainerMetricsComponent implements OnInit {
   }
 
 
-
-  proba(data: any) {
+  getMetricsFromInput(data: any) {
 
     this.CONTAINERS_DATA = this.getContainerInfo(data)
     this.dataSource.data = this.CONTAINERS_DATA
@@ -114,11 +119,15 @@ export class ContainerMetricsComponent implements OnInit {
 
   getCpuChartSeries(data: any) {
 
+    const sortedArray = data.cpu_percent.sort((a, b) => {
+      return moment(a.time_stamp).diff(b.time_stamp);
+    });
+
     return [{
       // Return the new object structure
       name: data._id,
-      series: data.cpu_percent.map(item => ({
-        name: item.time_stamp,
+      series: sortedArray.map(item => ({
+        name: moment.utc(item.time_stamp).format('MMMM D, YYYY'),
         value: item.percent
       }))
     }];
@@ -126,11 +135,15 @@ export class ContainerMetricsComponent implements OnInit {
 
   getMemoryChartSeries(data: any) {
 
+    const sortedArray = data.mem_percent.sort((a, b) => {
+      return moment(a.time_stamp).diff(b.time_stamp);
+    });
+
     return [{
       // Return the new object structure
       name: data._id,
-      series: data.cpu_percent.map(item => ({
-        name: item.time_stamp,
+      series: sortedArray.map(item => ({
+        name: moment.utc(item.time_stamp).format('MMMM D, YYYY'),
         value: item.percent
       }))
     }];
@@ -143,7 +156,6 @@ export class ContainerMetricsComponent implements OnInit {
   // Code will be updated once dockerhost attribute is included.
   getContainerInfo(data: any) {
 
-
     return [{
       // Return the new object structure
       name: data.hostname,
@@ -152,6 +164,21 @@ export class ContainerMetricsComponent implements OnInit {
       host: "DockerHost",
 
     }];
+  }
+
+  secondsToDhms(seconds) {
+    seconds = Number(seconds);
+    var d = Math.floor(seconds / (3600 * 24));
+    var h = Math.floor(seconds % (3600 * 24) / 3600);
+    var m = Math.floor(seconds % 3600 / 60);
+    var s = Math.floor(seconds % 60);
+
+    var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+
+    return dDisplay + hDisplay + mDisplay + sDisplay;
   }
 }
 
